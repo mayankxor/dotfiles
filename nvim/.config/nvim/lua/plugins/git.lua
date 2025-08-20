3152fcec7a0cde95a701f2d82a825a75edffe890
mayankxor <mayankxor@gmail.com>
2025-08-20 18:41:48 +0530

    Configured git signs and moved lazygit to git.lua

---
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -0,0 +1,139 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		enabled = true,
+		config = function()
+			require("gitsigns").setup({
+				signs = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				signs_staged = {
+					add = { text = "┃" },
+					change = { text = "┃" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+					untracked = { text = "┆" },
+				},
+				signs_staged_enable = true,
+				signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+				numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+				linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+				word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+				watch_gitdir = {
+					follow_files = true,
+				},
+				auto_attach = true,
+				attach_to_untracked = false,
+				current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = true,
+					virt_text_priority = 100,
+					use_focus = true,
+				},
+				current_line_blame_formatter = "<author>, <author_time:%R> - <summary>",
+				sign_priority = 6,
+				update_debounce = 100,
+				status_formatter = nil, -- Use default
+				max_file_length = 40000, -- Disable if file is longer than this (in lines)
+				preview_config = {
+					-- Options passed to nvim_open_win
+					style = "minimal",
+					relative = "cursor",
+					row = 0,
+					col = 1,
+				},
+				on_attach = function(bufnr)
+					local gitsigns = require("gitsigns")
+
+					local function map(mode, l, r, opts)
+						opts = opts or {}
+						opts.buffer = bufnr
+						vim.keymap.set(mode, l, r, opts)
+					end
+
+					map("n", "]c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "]c", bang = true })
+						else
+							gitsigns.nav_hunk("next")
+						end
+					end, { desc = "Go to next hunk" })
+
+					map("n", "[c", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "[c", bang = true })
+						else
+							gitsigns.nav_hunk("prev")
+						end
+					end, { desc = "Go to previous hunk" })
+
+					map("n", "<leader>hs", gitsigns.stage_hunk, { desc = "Stage hunk" })
+					map("n", "<leader>hr", gitsigns.reset_hunk, { desc = "reset hunk" })
+					map("v", "<leader>hs", function()
+						gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, { desc = "Stage hunk" })
+					map("v", "<leader>hr", function()
+						gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+					end, { desc = "Reset hunk" })
+
+					map("n", "<leader>hS", gitsigns.stage_buffer, { desc = "Stage buffer" })
+					map("n", "<leader>hR", gitsigns.reset_buffer, { desc = "Reset buff" })
+					map("n", "<leader>hp", gitsigns.preview_hunk, { desc = "Preview hunk" })
+					map("n", "<leader>hi", gitsigns.preview_hunk_inline, { desc = "Preview hunk inline" })
+					map("n", "<leader>hb", function()
+						gitsigns.blame_line({ full = true })
+					end, { desc = "blame line" })
+					map("n", "<leader>hd", gitsigns.diffthis, { desc = "Diff hunk" })
+					map("n", "<leader>hD", function()
+						gitsigns.diffthis("~")
+					end)
+					map("n", "<leader>hQ", function()
+						gitsigns.setqflist("all")
+					end)
+					map("n", "<leader>hq", gitsigns.setqflist)
+
+					map(
+						"n",
+						"<leader>tb",
+						gitsigns.toggle_current_line_blame,
+						{ desc = "Toggle line blame for current line" }
+					)
+					map("n", "<leader>twd", gitsigns.toggle_word_diff, { desc = "Toggle word diff" })
+
+					map({ "o", "x" }, "ih", gitsigns.select_hunk, { desc = "Select hunk" })
+				end,
+			})
+		end,
+	},
+	{
+		"kdheepak/lazygit.nvim",
+		enabled = true,
+		lazy = true,
+		cmd = {
+			"LazyGit",
+			"LazyGitConfig",
+			"LazyGitCurrentFile",
+			"LazyGitFilter",
+			"LazyGitFilterCurrentFile",
+		},
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			{ "<leader>lg", "<cmd>LazyGit<CR>", desc = "Open lazygit" },
+			{ "<leader>gl", "<cmd>LazyGitLog<CR>", desc = "LazyGit logs" },
+		},
+		-- An extension of LazyGit is available for telescope but requires lazy=false,
+		-- TODO
+		-- Check if the lazy=true feature is available for telescope extension
+	},
+}
