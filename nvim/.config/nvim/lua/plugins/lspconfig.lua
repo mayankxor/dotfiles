9cbcdcda4d9990e8a53298a435b5606997b2f387
mayankxor <mayankxor@gmail.com>
2025-08-21 20:29:44 +0530

    Configured lspkind
    Also lsp-nvim-cmp now loads when attached to a buffer

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -64,6 +64,7 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		enabled = true,
+		event = { "BufReadPre", "BufEnter" },
 		config = function()
 			-- local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
@@ -198,6 +199,7 @@ return {
 		event = "InsertEnter",
 		dependencies = {
 			"hrsh7th/cmp-buffer",
+			"onsails/lspkind.nvim",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-calc",
@@ -215,6 +217,7 @@ return {
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
 
+			local lspkind = require("lspkind")
 			-- To autocomplete the last remaining completion using <TAB>
 			local has_words_before = function()
 				unpack = unpack or table.unpack
@@ -229,17 +232,65 @@ return {
 						require("luasnip").lsp_expand(args.body)
 					end,
 				},
+				-- formatting = {
+				-- 	format = function(entry, vim_item)
+				-- 		vim_item.menu = ({
+				-- 			nvim_lsp = "[LSP]",
+				-- 			luasnip = "[Snippet]",
+				-- 			buffer = "[Buffer]",
+				-- 			path = "[Path]",
+				-- 			codecompanion = "[CodeCompanion]",
+				-- 		})[entry.source.name]
+				-- 		return vim_item
+				-- 	end,
+				-- },
 				formatting = {
-					format = function(entry, vim_item)
-						vim_item.menu = ({
-							nvim_lsp = "[LSP]",
-							luasnip = "[Snippet]",
-							buffer = "[Buffer]",
-							path = "[Path]",
-							codecompanion = "[CodeCompanion]",
-						})[entry.source.name]
-						return vim_item
-					end,
+					format = lspkind.cmp_format({
+						mode = "symbol", -- show only symbol annotations
+						maxwidth = {
+							-- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+							-- can also be a function to dynamically calculate max width such as
+							-- menu = function() return math.floor(0.45 * vim.o.columns) end,
+							menu = 50, -- leading text (labelDetails)
+							abbr = 50, -- actual suggestion item
+						},
+						ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+						show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+
+						-- The function below will be called before any actual modifications from lspkind
+						-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+						before = function(entry, vim_item)
+							-- ...
+							return vim_item
+						end,
+					}),
+					symbol_map = {
+						Text = "󰉿",
+						Method = "󰆧",
+						Function = "󰊕",
+						Constructor = "",
+						Field = "󰜢",
+						Variable = "󰀫",
+						Class = "󰠱",
+						Interface = "",
+						Module = "",
+						Property = "󰜢",
+						Unit = "󰑭",
+						Value = "󰎠",
+						Enum = "",
+						Keyword = "󰌋",
+						Snippet = "",
+						Color = "󰏘",
+						File = "󰈙",
+						Reference = "󰈇",
+						Folder = "󰉋",
+						EnumMember = "",
+						Constant = "󰏿",
+						Struct = "󰙅",
+						Event = "",
+						Operator = "󰆕",
+						TypeParameter = "",
+					},
 				},
 				window = {
 					completion = cmp.config.window.bordered(),
