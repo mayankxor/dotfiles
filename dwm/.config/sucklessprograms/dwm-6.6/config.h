db30eea53e3a18456e856be1c95583cc2481001a
mayankxor <mayankxor@gmail.com>
2025-12-08 19:32:08 +0530

    changed modkey to windows

--- dwm/.config/sucklessprograms/dwm-6.6/config.h
+++ dwm/.config/sucklessprograms/dwm-6.6/config.h
@@ -7,13 +7,13 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
-static unsigned int baralpha        = 0x64;
+static unsigned int baralpha        = 0x96;
 static unsigned int borderalpha     = OPAQUE;
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:style=Regular:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
+static const char col_gray3[]       = "#ffffff";
+static const char col_gray4[]       = "#ffffff";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -49,7 +49,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
