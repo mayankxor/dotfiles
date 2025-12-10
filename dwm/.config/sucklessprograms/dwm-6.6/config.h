a0fafcf33fb6fc4e3d9d76b93d6f45664e3e1303
mayankxor <mayankxor@gmail.com>
2025-12-10 13:34:37 +0530

    Added keyboad volume control functionality volume up, down and mute fixed using `pactl`

--- dwm/.config/sucklessprograms/dwm-6.6/config.h
+++ dwm/.config/sucklessprograms/dwm-6.6/config.h
@@ -63,6 +63,9 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-i", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *incvol[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL};
+static const char *decvol[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL};
+static const char *mutevol[] = {"pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -89,6 +92,9 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{0, XF86XK_AudioLowerVolume, spawn, {.v=decvol}},
+	{0,XF86XK_AudioRaiseVolume, spawn, {.v=incvol}},
+	{0, XF86XK_AudioMute, spawn, {.v=mutevol}},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
