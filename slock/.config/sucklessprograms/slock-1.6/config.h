033b0151d5a2cc90930a5a7ed056cbe927d4b5a8
mayankxor <mayankxor@gmail.com>
2026-01-22 21:38:33 +0530

    slock init

---
+++ slock/.config/sucklessprograms/slock-1.6/config.h
@@ -0,0 +1,13 @@
+/* user and group to drop privileges to */
+static const char *user  = "xor";
+static const char *group = "xor";
+
+static const char *colorname[NUMCOLS] = {
+	[INIT] =   "black",     /* after initialization */
+	[INPUT] =  "#005577",   /* during input */
+	[INPUT_ALT] = "#227799", /* during input, second color */
+	[FAILED] = "#CC3333",   /* wrong password */
+};
+
+/* treat a cleared input like a wrong password (color) */
+static const int failonclear = 1;
