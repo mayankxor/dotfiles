32166a05cc41b11704db5cc345907138977a8382
mayankxor <mayankxor@gmail.com>
2026-01-27 20:08:55 +0530

    modules reorder

--- dwmblocks-async/.config/dwmblocks-async/config.h
+++ dwmblocks-async/.config/dwmblocks-async/config.h
@@ -5,7 +5,7 @@
 #define DELIMITER "  "
 
 // Maximum number of Unicode characters that a block can output.
-#define MAX_BLOCK_OUTPUT_LENGTH 20
+#define MAX_BLOCK_OUTPUT_LENGTH 17
 
 // Control whether blocks are clickable.
 #define CLICKABLE_BLOCKS 1
@@ -18,10 +18,8 @@
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
 #define BLOCKS(X)             \
-    X("", "/home/xor/.config/dwmblocks-async/scripts/time", 1, 4)  \
+    X("", "/home/xor/.config/dwmblocks-async/scripts/packagestoupdate", 3600, 18) \
     X("", "/home/xor/.config/dwmblocks-async/scripts/memory", 30, 5) \
-    X("", "/home/xor/.config/dwmblocks-async/scripts/battery", 10, 6) \
-    X("", "/home/xor/.config/dwmblocks-async/scripts/volume", 0, 7) \
     X("", "/home/xor/.config/dwmblocks-async/scripts/wifi", 10, 8) \
     X("", "/home/xor/.config/dwmblocks-async/scripts/bluetooth", 10, 9) \
     X("", "/home/xor/.config/dwmblocks-async/scripts/weather", 30, 10) \
@@ -32,8 +30,10 @@
     X("", "/home/xor/.config/dwmblocks-async/scripts/todolist", 30, 15) \
     X("", "/home/xor/.config/dwmblocks-async/scripts/uptime", 30, 16) \
     X("", "/home/xor/.config/dwmblocks-async/scripts/temp", 30, 17) \
-    X("", "/home/xor/.config/dwmblocks-async/scripts/packagestoupdate", 3600, 18) \
     X("", "/home/xor/.config/dwmblocks-async/scripts/moonphases", 30, 19) \
+    X("", "/home/xor/.config/dwmblocks-async/scripts/volume", 0, 7) \
+    X("", "/home/xor/.config/dwmblocks-async/scripts/battery", 10, 6) \
+    X("", "/home/xor/.config/dwmblocks-async/scripts/time", 1, 4)  \
 
 
 #endif  // CONFIG_H
