a14ddd40a5e8301fcda62c95c19bd092cfb52853
mayankxor <mayankxor@gmail.com>
2026-05-01 01:34:24 +0530

    yazi:added `git` package

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,6 +1,7 @@
 function Linemode:size_and_mtime()
 	local time = math.floor(self._file.cha.mtime or 0)
-	if time == 0 then time = ""
+	if time == 0 then
+		time = ""
 	elseif os.date("%Y", time) == os.date("%Y") then
 		time = os.date("%b%d-%Y-%H:%M", time)
 	else
@@ -27,4 +28,17 @@ require("bookmarks"):setup({
 			delete_all = "Deleted all bookmarks",
 		},
 	},
+})
+
+th.git = th.git or {}
+th.git.untracked = ui.Style():bold()
+th.git.unknown = ui.Style():bold()
+th.git.modified = ui.Style():bold()
+th.git.added = ui.Style():bold()
+th.git.ignored = ui.Style():bold()
+th.git.deleted = ui.Style():bold()
+th.git.updated = ui.Style():bold()
+th.git.clean = ui.Style():bold()
+require("git"):setup({
+	order = 1500,
 })
