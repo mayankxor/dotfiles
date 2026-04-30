832514ad955d118c5309bef581c67249d3f1be1b
mayankxor <mayankxor@gmail.com>
2026-05-01 01:14:06 +0530

    yazi:formatted `init.lua`

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,7 +1,6 @@
 function Linemode:size_and_mtime()
 	local time = math.floor(self._file.cha.mtime or 0)
-	if time == 0 then
-		time = ""
+	if time == 0 then time = ""
 	elseif os.date("%Y", time) == os.date("%Y") then
 		time = os.date("%b%d-%Y-%H:%M", time)
 	else
