516bd660b2b06fb1cdb6afdc8df328c6e20b33a4
mayankxor <mayankxor@gmail.com>
2026-05-03 20:16:39 +0530

    nvim:added text yank highlight autocommand

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -4,3 +4,10 @@ vim.api.nvim_create_autocmd("BufWinEnter", {
     vim.opt_local.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     end,
 })
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight parts of lines being yanked",
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
--- nvim/.config/nvim/lua/config/init.lua
+++ nvim/.config/nvim/lua/config/init.lua
@@ -1,6 +1,7 @@
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
+require("config.autocmds")
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function()
 		require("lazy").update({ show = false })
