6f82c1f02d91b757397139bf2b20d633daffac13
Mayank Rajput <mayankxor@gmail.com>
2026-06-05 21:30:20 +0530

    i3blocks:added lots of stuff idk

---
+++ i3blocks/.config/i3blocks/i3blocks-config.h
@@ -0,0 +1,26 @@
+/* i3blocks-config.h.  Generated from i3blocks-config.h.in by configure.  */
+/* i3blocks-config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Name of package */
+#define PACKAGE "i3blocks"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "i3blocks"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "i3blocks 1.5"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "i3blocks"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.5"
+
+/* Version number of package */
+#define VERSION "1.5"
---
+++ i3blocks/.config/i3blocks/scripts/volume.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Left click to mute
+if [ "$BLOCK_BUTTON" = "1" ]; then
+  pactl set-sink-mute @DEFAULT_SINK@ toggle
+fi
+
+# Scroll = Volume Change
+if [[ "$BLOCK_BUTTON" = "4" ]]; then
+  pactl set-sink-volume @DEFAULT_SINK@ +5%
+fi
+if [ "$BLOCK_BUTTON" = "5" ]; then
+    pactl set-sink-volume @DEFAULT_SINK@ -5%
+fi
+
+# Read mute status
+MUTED=$(pactl get-sink-mute @DEFAULT_SINK@ | awk '{print $2}')
+
+# Read current volume
+VOLUME=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}')
+VOLUME="${VOLUME//%/}" # strip "%"
+VOLUME="${VOLUME//[^0-9]/}" # Convert string to int
+
+# Output volume level(muted icon incase muted)
+if [[ "$MUTED" = "yes" ]]; then
+  ICON=" "
+elif [[ "$VOLUME" -le 5 ]]; then
+  ICON=" "
+elif [[ "$VOLUME" -le 30 ]]; then
+  ICON=" "
+else
+  ICON=" "
+fi
+echo "$ICON$VOLUME"
