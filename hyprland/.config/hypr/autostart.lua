49cdf70b2b95811a515a23b84d9587bd7eca1902
Mayank Rajput <mayankxor@gmail.com>
2026-06-05 20:50:09 +0530

    wpaperd:init(with hyprpolkit) And autostart with hyprland

--- hyprland/.config/hypr/autostart.lua
+++ hyprland/.config/hypr/autostart.lua
@@ -2,8 +2,10 @@
 
 hl.on("hyprland.start", function()
   hl.exec_cmd("waybar")
-  hl.exec_cmd("hyprpaper")
+  -- hl.exec_cmd("hyprpaper")
+  hl.exec_cmd("wpaperd")
   hl.exec_cmd("hypridle")
   hl.exec_cmd("hyprsunset")
   hl.exec_cmd("swaync")
+  hl.exec_cmd("systemctl --user status hyprpolkitagent")
 end)
