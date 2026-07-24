b76197127b262f927cd068317aa4c23ac366e587
Mayank Rajput <mayankxor@gmail.com>
2026-07-24 17:41:50 +0530

    dwm:updated scripts name

--- dwm/.config/sucklessprograms/dwm-6.6/config.h
+++ dwm/.config/sucklessprograms/dwm-6.6/config.h
@@ -169,7 +169,7 @@ static Keychord *keychords[] = {
     &((Keychord){1,
                  {{MODKEY | ShiftMask, XK_t}},
                  spawn,
-                 SHCMD("~/.scripts/todo-dmenu")}),
+                 SHCMD("~/.scripts/dmenu/todo-dmenu")}),
     &((Keychord){
         2, {{MODKEY | ShiftMask, XK_o}, {0, XK_b}}, spawn, SHCMD("$BROWSER")}),
     &((Keychord){
@@ -189,7 +189,7 @@ static Keychord *keychords[] = {
     &((Keychord){1,
                  {{MODKEY | ShiftMask, XK_x}},
                  spawn,
-                 SHCMD("~/.scripts/dmenukill.sh")}),
+                 SHCMD("~/.scripts/dmenu/dmenukill")}),
 
     // In case of no numpad symbols, use:
     // XK_KP_Add -> XK_equal and XK_KP_Subtract -> XK_minus
@@ -197,15 +197,15 @@ static Keychord *keychords[] = {
     &((Keychord){1,
                  {{MODKEY | ShiftMask, XK_KP_Add}},
                  spawn,
-                 SHCMD("~/.scripts/changeopacity.sh +1")}),
+                 SHCMD("~/.scripts/X/changeopacity +1")}),
     &((Keychord){1,
                  {{MODKEY | ShiftMask, XK_KP_Subtract}},
                  spawn,
-                 SHCMD("~/.scripts/changeopacity.sh -1")}),
+                 SHCMD("~/.scripts/X/changeopacity -1")}),
     &((Keychord){1,
                  {{MODKEY | ShiftMask, XK_Escape}},
                  spawn,
-                 SHCMD("~/.scripts/xkiller.sh")}),
+                 SHCMD("~/.scripts/X/xkiller")}),
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
             TAGKEYS(XK_9, 8)
