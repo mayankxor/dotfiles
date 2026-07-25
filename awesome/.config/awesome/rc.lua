22dabcf161ab933d0262b6252d3332587222f2a2
Mayank Rajput <mayankxor@gmail.com>
2026-07-25 20:08:41 +0530

    awesome: modularized mousebinds

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -2,38 +2,18 @@
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
 
--- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
 require("awful.autofocus")
--- Widget and layout library
--- Theme handling library
 local beautiful = require("beautiful")
--- Notification library
 local menubar = require("menubar")
--- Enable hotkeys help widget for VIM and other apps
--- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
-
 require("errorhandling")
-
--- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
-
--- This is used later as the default terminal and editor to run.
 TERMINAL = "st"
 EDITOR = os.getenv("EDITOR") or "vi"
 EDITOR_CMD = TERMINAL .. " -e " .. EDITOR
-
--- Default modkey.
--- Usually, Mod4 is the key with a logo between Control and Alt.
--- If you do not like this or do not have such a key,
--- I suggest you to remap Mod4 to another key using xmodmap or other tools.
--- However, you can use another modifier like Mod1, but it may interact with others.
 MODKEY = "Mod4"
-
--- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
   awful.layout.suit.floating,
   awful.layout.suit.tile,
@@ -52,21 +32,9 @@ awful.layout.layouts = {
   -- awful.layout.suit.corner.sw,
   -- awful.layout.suit.corner.se,
 }
--- }}}
-
-local mymainmenu = require("menu")
 menubar.utils.terminal = TERMINAL -- Set the terminal for applications that require it
-
--- Keyboard map indicator and switcher
-
 require("wibar.wibar")
-
--- {{{ Mouse bindings
-root.buttons(gears.table.join(
-  awful.button({}, 3, function() mymainmenu:toggle() end),
-  awful.button({}, 4, awful.tag.viewnext),
-  awful.button({}, 5, awful.tag.viewprev)
-))
+require("mousebinds")
 require("keybinds")
 require("rules")
 require("signals")
