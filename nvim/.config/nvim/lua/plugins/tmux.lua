a70ec48202977da658badaecfc8c7708b694b56d
Mayank Rajput <mayankxor@gmail.com>
2026-09-12 13:43:27 +0000

    nvim:disable vim-tpipeline Its not the time for it

--- nvim/.config/nvim/lua/plugins/tmux.lua
+++ nvim/.config/nvim/lua/plugins/tmux.lua
@@ -1,18 +1,24 @@
 return {
-  "christoomey/vim-tmux-navigator",
-  cmd = {
-    "TmuxNavigateLeft",
-    "TmuxNavigateDown",
-    "TmuxNavigateUp",
-    "TmuxNavigateRight",
-    "TmuxNavigatePrevious",
-    "TmuxNavigatorProcessList",
+  {
+    "vimpostor/vim-tpipeline",
+    enabled = false,
   },
-  keys = {
-    { "<m-h>", "<cmd>TmuxNavigateLeft<cr>" },
-    { "<m-j>", "<cmd>TmuxNavigateDown<cr>" },
-    { "<m-k>", "<cmd>TmuxNavigateUp<cr>" },
-    { "<m-l>", "<cmd>TmuxNavigateRight<cr>" },
-    { "<m-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+      "TmuxNavigatorProcessList",
+    },
+    keys = {
+      { "<m-h>", "<cmd>TmuxNavigateLeft<cr>" },
+      { "<m-j>", "<cmd>TmuxNavigateDown<cr>" },
+      { "<m-k>", "<cmd>TmuxNavigateUp<cr>" },
+      { "<m-l>", "<cmd>TmuxNavigateRight<cr>" },
+      { "<m-\\>", "<cmd>TmuxNavigatePrevious<cr>" },
+    },
   },
 }
