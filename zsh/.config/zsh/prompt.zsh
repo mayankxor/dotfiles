93acb2ddd958b21c5293dc92e8e3f4e34b965d76
Mayank Rajput <mayankxor@gmail.com>
2026-09-23 19:54:22 +0530

    zsh: merged the general prompt with termux prompt

--- zsh/.config/zsh/prompt.zsh
+++ zsh/.config/zsh/prompt.zsh
@@ -306,10 +306,5 @@ function battery_level_gauge() {
 ####### /BATTERY MODULE #######
 
 # PS1='[%F{#2fc6b2}%n@%M%f %F{#2fc66c}%0~%f]${vcs_info_msg_0_}${NEWLINE}%(!.#.$) '
-if [[ -n "$TERMUX_VERSION" ]]; then
-  PS1='[%F{#2fc66c}%0~%f]${vcs_info_msg_0_}%(!.#.$) '
-  RPS1='%F{green}  %f%j'
-else
   PS1='%F{$base16[base06]}%0~%f %F{$base16[base0F]}${vcs_info_msg_0_}%f$NEWLINE%(!.μ.λ) '
   # RPS1='[$(battery_pct_prompt)]%?  %* %F{green}  %f%j'
-fi
